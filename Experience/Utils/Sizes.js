633eb4a01e4a05321170267a5dacdea603f0319f
Phuc Le <phuclevinh2000@gmail.com>
2022-12-28 23:48:11 +0200

    Create the sceleton for the project

---
+++ Experience/Utils/Sizes.js
@@ -0,0 +1,16 @@
+export default class Sizes {
+  constructor() {
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+    this.aspect = this.width / this.height;
+    this.pixelRatio = Math.min(window.devicePixelRatio, 2);
+
+    // Resize event handler
+    window.addEventListener('resize', () => {
+      this.width = window.innerWidth;
+      this.height = window.innerHeight;
+      this.aspect = this.width / this.height;
+      this.pixelRatio = Math.min(window.devicePixelRatio, 2);
+    });
+  }
+}
